1812896290fae6e123ad8439a98bd00174745d51
Anand Shekhawat <anandsingh215@yahoo.com>
2024-09-03 22:09:35 +0530

    rename assistant bot

--- pyUltroid/_misc/_assistant.py
+++ pyUltroid/_misc/_assistant.py
@@ -21,10 +21,10 @@ from . import append_or_update, owner_and_sudos
 OWNER = ultroid_bot.full_name
 
 MSG = f"""
-**Ultroid - UserBot**
+**Shekhawat2 - UserBot**
 ➖➖➖➖➖➖➖➖➖➖
 **Owner**: [{OWNER}](tg://user?id={ultroid_bot.uid})
-**Support**: @TeamUltroid
+**Support**: @Shekhawat2
 ➖➖➖➖➖➖➖➖➖➖
 """
 
@@ -32,9 +32,9 @@ IN_BTTS = [
     [
         Button.url(
             "Repository",
-            url="https://github.com/TeamUltroid/Ultroid",
+            url="https://github.com/Shekhawat2/Ultroid",
         ),
-        Button.url("Support", url="https://t.me/UltroidSupportChat"),
+        Button.url("Support", url="https://t.me/Shekhawat2"),
     ]
 ]
 
